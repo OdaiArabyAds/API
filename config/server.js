d78d76803b0c4f070e38d085661f38f370542a9b
OdaiArabyAds <91122482+OdaiArabyAds@users.noreply.github.com>
2022-11-09 13:30:09 +0200

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "127.0.0.1"),
+  host: env("HOST", process.env.HOST ||"127.0.0.1"),
   port: env.int("PORT", process.env.PORT || 8082),
   admin: {
     auth: {
